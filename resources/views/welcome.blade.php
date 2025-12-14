e6f8826a31b7a46dbcdd41562f815b085d29be9f
Jaydee Ramos <jaydeer79@gmail.com>
2025-12-14 23:04:32 +0530

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Practice</title>
+</head>
+<body>
+    <h1>Welcome to Laravel Practice</h1>
+    <p>Click the button below to view the list of ninjas</p>
+    <a href="" class="btn">
+        Find Ninjas!
+    </a>
+</body>
+</html>
