628f4a5858a69444656ee196905ffa97dd1950b3
Jaydee Ramos <jaydeer79@gmail.com>
2025-12-23 13:22:48 +0530

    added create_todos_table

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,13 +3,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Heroes Network</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <header>
         <nav>
-            <h1>Heroes Network</h1>
-            <a href="/heroes">Heroes</a>
-            <a href="/heroes/create">Create New Hero</a>
+            <h1 class="">Heroes Network</h1>
+            <a href="/heroes" class="hover:text-red-600">Heroes</a>
+            <a href="/heroes/create" class="hover:text-red-600">Create New Hero</a>
         </nav>
     </header>
     
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,19 @@
-<x-layout>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heroes Network</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="text-center px-8 py-12">
     <h1>Welcome to Laravel Practice</h1>
     <p>Click the button below to view the list of ninjas</p>
-    <a href="/heroes" class="btn">
+    <br>
+    <a href="/heroes" class="btn border-2 border-gray-400 hover:border-red-600">
         Find Heroes!
     </a>
-</x-layout>
+</body>
+
+</html>
