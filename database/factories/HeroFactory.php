065006cb40a4f038da501a2902b991e2b183f2c0
Jaydee Ramos <jaydeer79@gmail.com>
2025-12-24 09:22:36 +0530

    added a fake api to generate random att and use Hero::factory

--- database/factories/HeroFactory.php
+++ database/factories/HeroFactory.php
@@ -14,10 +14,12 @@ class HeroFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
+    public function definition(): array    //Hero::factory()->count(5)->create(); => creates 5 heroes with these attributes
     {
         return [
-            //
+            'name' => fake()->name(), // Generate a fake name
+            'skill' => fake()->numberBetween(0, 100), // Generate a fake skill level between 0 and 100
+            'quirk' => fake()->word(), // Generate a fake quirk
         ];
-    }
+    }   //all the generated will be stored in the database
 }
