562c1e1c5d764a70ddcb742e177ba11905caf816
Jaydee Ramos <jaydeer79@gmail.com>
2025-12-24 11:28:24 +0530

    seed heroes from the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,8 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        $this->call([
+            HeroSeeder::class,
+        ]); //call the HeroSeeder to seed heroes
     }
 }
--- database/seeders/HeroSeeder.php
+++ database/seeders/HeroSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Hero;
 
 class HeroSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class HeroSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Hero::factory()->count(50)->create();   // Create 10 heroes using the factory
     }
 }
